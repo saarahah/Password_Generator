4adbfc92581c9f39f8d797093208df4b2da8fb3b
saarahah <sarahlferguson07@gmail.com>
2020-06-11 23:33:34 -0400

    Update script.js
    for some reason my git status says up to date when it is not so i am manually uploading this commit

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+//global variables for strings and random array
+var generateBtn = document.querySelector("#generate");
+//string of lowercase
 var letterString="abcdefghijklmnopqrstuvwxyz";
- var numberString="1234567890";
+//string of numbers
+var numberString="1234567890";
+//string of special characters
 var characterString="!@#$%^&*()";
+//string of uppercase characters
 var letterUCString="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//check to make sure at least 1 option chosen after character count
 var stringCheck = 0;
+//array which the random chosen letters are pushed to
 var randomArray =[];
 
+//declared these variables globally for reset
+var capitalLetters;
+var specialcharacters;
+var numericCharacters;
+var lowercaseCharacters;
 
+//creates function and asks user for numOfCharacters input for variable
 function generatePassword(){
   var testPassword = "";
-  var numOfCharacters= prompt ("how many characters")
-  
+  var numOfCharacters= prompt ("how many characters");
 
-  if (numOfCharacters < 8 ){
+//check that the character type and length is correct
+  if (numOfCharacters < 8){
     alert ("needs to be at least 8 char");
   }
-      else if (numOfCharacters>128){
-        alert("needs to be less than 129 characters")
+//larger number of characters
+     else if (numOfCharacters>128){
+        alert("needs to be less than 129 characters");
       }
-      else{
-    
+//input is not a number
+       else if (isNaN(numOfCharacters)){
+       alert("not a number");
+       
+        } else{
+          // console.log("test" + numOfCharacters);
+
+  //if correct move onto other confirmations re: characters/capitals
   var specialcharacters=confirm ("will there be special characters");
-  if(specialcharacters===true){
+    if(specialcharacters===true){
     stringCheck ++;
   }
+  //confirm whether there will be numbers if yes add to 
   var numericCharacters=confirm ("will there be numbers");
   if(numericCharacters===true){
     stringCheck ++;
   }
 
+  //confirms lowercase letters
   var lowercaseCharacters=confirm ("will there be lowercase characters");
   if(lowercaseCharacters===true){
     stringCheck ++;
   }
 
+  //confirms capital letters
   var capitalLetters=confirm ("will there be capitals");
   if(capitalLetters===true){
     stringCheck ++;
   }
-  if (stringCheck>0){
-    for (let i=0; i< numOfCharacters; i++){
-
+  if (stringCheck > 0 ){
+    for (let i=0; i < numOfCharacters; i++){
+      var randomNumber = Math.floor(Math.random() * 10)
+      var randomLetter =letterString[Math.floor(Math.random() * letterString.length)]
+      var randomUppercase =letterUCString[Math.floor(Math.random() * letterUCString.length)]
+      var randomCharacter = characterString[Math.floor(Math.random() * characterString.length)]
       
-      var randomNumber = Math.floor(Math.random() *10)
-      var randomLetter =letterString[Math.floor(Math.random() *letterString.length)]
-      var randomUppercase =letterUCString[Math.floor(Math.random() *letterUCString.length)]
-      var randomCharacter = characterString[Math.floor(Math.random() *characterString.length)]
-      // var randomArray =[randomUppercase, randomNumber, randomLetter, randomCharacter];
-      
-      
-
       if(specialcharacters===true){
         randomArray.push(randomCharacter);
       }
@@ -67,28 +85,37 @@ function generatePassword(){
         randomArray.push(randomNumber);
       }
 
-      var random =randomArray[Math.floor(Math.random() *randomArray.length)]
+      var random =randomArray[Math.floor(Math.random() * randomArray.length)]
+
       testPassword += random;
   }
-  return testPassword
-
+   return testPassword
 
   }else{
     alert("you must have at least one character option")
+    }
   }
 }
-}
-
-
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+resetValues();
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//resetting the values to initial so a new password can be made without reloading
+function resetValues(){
+  randomArray =[];
+  stringCheck=0;
+   capitalLetters ===false;
+   specialcharacters===false;
+   numericCharacters===false;
+   lowercaseCharacters===false;
+  // console.log("reset this bitch");
+}
